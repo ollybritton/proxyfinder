a3f597a20cc7bc3201e2986b0c372cf118695daf
Olly Britton <ollybritton@gmail.com>
2019-05-08 20:22:45 +0100

    Fix bug where duplicate proxies can exist

--- proxy.go
+++ proxy.go
@@ -16,12 +16,15 @@ func NewProxy(url url.URL, provider string) Proxy {
 }
 
 // UniqueProxies returns a list of proxies with all duplicates removed.
+// Instead of just using the plain Proxy type, it only checks if the URL matches.
+// This is because a proxy that is identical to another from a different provider will not match because the provider will be different, although fundementally it is the same proxy.
 func UniqueProxies(proxies []Proxy) []Proxy {
-	keys := make(map[Proxy]bool)
+	keys := make(map[url.URL]bool)
 	list := []Proxy{}
+
 	for _, entry := range proxies {
-		if _, value := keys[entry]; !value {
-			keys[entry] = true
+		if _, value := keys[entry.URL]; !value {
+			keys[entry.URL] = true
 			list = append(list, entry)
 		}
 	}
